4bbf4793520bac662f3f3fa6905d9cb0be2d2827
nastyabag <nbagaynikova@mail.ru>
2021-12-06 20:10:13 +0800

    add circles and score

---
+++ Lab5/Objects/MyCircle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.Objects
+{
+    public class MyCircle : BaseObject
+    {
+        public int Timer;
+
+        public Action<MyCircle> ;
+
+        public MyCircle(float x, float y, float angle) : base(x, y, angle)
+        {
+        }
+
+        public override void Render(Graphics g)
+        {
+            g.FillEllipse(
+                new SolidBrush(Color.Green),
+                -20, -20, 40, 40
+                );
+        }
+
+        public override GraphicsPath GetGraphicsPath()
+        {
+            var path = base.GetGraphicsPath();
+            path.AddEllipse(-20, -20, 40, 40);
+            return path;
+        }
+
+        public void DecreaseTimer()
+        {
+            Timer--;
+            if (Timer < 0)
+            {
+
+            }
+        }
+    }
+}
