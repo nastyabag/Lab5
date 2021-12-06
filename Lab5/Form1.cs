da751d6541f68b6a26a9c8bd956a2674f12b596e
nastyabag <nbagaynikova@mail.ru>
2021-12-06 20:01:03 +0800

    manual

---
+++ Lab5/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lab5.Objects;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        List<BaseObject> objects = new();
+        Player player;
+        Marker? marker;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+
+            player.OnOverlap += (p, obj) =>
+            {
+                txtLog.Text = $"[{DateTime.Now:HH:mm:ss:ff}] Игрок пересекся с {obj}\n" + txtLog.Text;
+            };
+
+            player.OnMarkerOverlap += (m) =>
+            {
+                objects.Remove(m);
+                marker = null;
+            };
+
+            Random random = new();
+            objects.Add(new MyCircle(random.Next(20, 580), random.Next(20, 480), 0));
+            objects.Add(new MyCircle(random.Next(20, 580), random.Next(20, 380), 0));
+
+            objects.Add(player);
+            
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.Clear(Color.White);
+
+            updatePlayer();
+
+            foreach (var obj in objects.ToList())
+            {
+                if (obj != player && player.Overlaps(obj, g))
+                {
+                    player.Overlap(obj);
+                    obj.Overlap(player);
+                }
+            }
+
+            foreach (var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pbMain.Invalidate();
+        }
+
+        private void updatePlayer()
+        {
+            if (marker != null)
+            {
+                float dx = marker.X - player.X;
+                float dy = marker.Y - player.Y;
+                float length = MathF.Sqrt(dx * dx + dy * dy);
+                dx /= length;
+                dy /= length;
+
+                player.vX += dx * 0.5f;
+                player.vY += dy * 0.5f;
+
+                player.Angle = 90 - MathF.Atan2(player.vX, player.vY) * 180 / MathF.PI;
+            }
+
+            player.vX += -player.vX * 0.1f;
+            player.vY += -player.vY * 0.1f;
+
+            player.X += player.vX;
+            player.Y += player.vY;
+        }
+
+        private void pbMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (marker == null)
+            {
+                marker = new Marker(0, 0, 0);
+                objects.Add(marker);
+            }
+
+            marker.X = e.X;
+            marker.Y = e.Y;
+        }
+    }
+}
---
+++ Lab5/Objects/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.Objects
+{
+    public class Player : BaseObject
+    {
+        public Action<Marker> OnMarkerOverlap;
+        public Action<MyCircle> OnCircleOverlap;
+
+        public float vX;
+        public float vY;
+        public Player(float x, float y, float angle) : base(x, y, angle)
+        {
+        }
+
+        public override void Render(Graphics g)
+        {
+            g.FillEllipse(
+                new SolidBrush(Color.DeepSkyBlue),
+                -15, -15, 30, 30
+                );
+
+            g.DrawEllipse(
+                new Pen(Color.Black, 2),
+                -15, -15,
+                30, 30
+                );
+
+            g.DrawEllipse(new Pen(Color.Black, 2), 0, 0, 25, 0);
+        }
+
+        public override GraphicsPath GetGraphicsPath()
+        {
+            var path = base.GetGraphicsPath();
+            path.AddEllipse(-15, -15, 30, 30);
+            return path;
+        }
+
+        public override void Overlap(BaseObject obj)
+        {
+            base.Overlap(obj);
+
+            if (obj is Marker)
+                OnMarkerOverlap(obj as Marker);
+            else if (obj is MyCircle)
+                OnCircleOverlap(obj as MyCircle);
+        }
+    }
+}
